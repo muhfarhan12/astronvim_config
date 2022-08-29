77173fc7a7fa6ed12eba8679c379c9c09815388a
Muhamad Farhan <muhamadfarhan91@gmail.com>
2022-08-29 10:54:58 +0700

    permulaan

---
+++ options.lua
@@ -0,0 +1,8 @@
+return {
+  opt = {
+    relativenumber = false,
+    -- tabstop = 4,
+    -- shiftwidth = 4,
+    conceallevel = 0,
+  },
+}
---
+++ plugins/ranger.lua
@@ -0,0 +1,7 @@
+local g = vim.g
+-- Ranger
+g.rnvimr_enable_ex = 1
+g.rnvimr_enable_picker = 1
+g.rnvimr_enable_bw = 1
+g.rnvimr_draw_border = 1
+
