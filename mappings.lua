388460a99be5075a4fb8af6268d70e69e9f90266
Muhamad Farhan <muhamadfarhan91@gmail.com>
2022-08-31 08:58:16 +0700

    kembali

--- mappings.lua
+++ mappings.lua
@@ -1,22 +1,27 @@
 return {
-  n = {
-    [";"] = { "<cmd>RnvimrToggle<cr>", desc = "Ranger" },
-    ["<leader>bb"] = { "<cmd>tabnew<cr>", desc = "New tab" },
-    ["<leader>bc"] = { "<cmd>BufferLinePickClose<cr>", desc = "Pick to close" },
-    ["<leader>bj"] = { "<cmd>BufferLinePick<cr>", desc = "Pick to jump" },
-    ["<leader>bt"] = { "<cmd>BufferLineSortByTabs<cr>", desc = "Sort by tabs" },
-    ["<leader>lf"] = { function() vim.lsp.buf.format { timeout_ms = 10000 } end, desc = "Format code" },
-    ["<leader>fs"] = {
-      function()
-        -- You can pass additional configuration to telescope to change theme, layout, etc.
-        require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown {
-          winblend = 10,
-          previewer = false,
-        })
-      end,
-      desc = "Fuzzily search in current buffer",
-    },
-    -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
-  },
+	n = {
+		[";"] = { "<cmd>RnvimrToggle<cr>", desc = "Ranger" },
+		["<leader>bb"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+		["<leader>bc"] = { "<cmd>BufferLinePickClose<cr>", desc = "Pick to close" },
+		["<leader>bj"] = { "<cmd>BufferLinePick<cr>", desc = "Pick to jump" },
+		["<leader>bt"] = { "<cmd>BufferLineSortByTabs<cr>", desc = "Sort by tabs" },
+		["<leader>lf"] = {
+			function()
+				vim.lsp.buf.formatting_sync(nil, 10000)
+			end,
+			desc = "Format code",
+		},
+		["<leader>fs"] = {
+			function()
+				-- You can pass additional configuration to telescope to change theme, layout, etc.
+				require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+					winblend = 10,
+					previewer = false,
+				}))
+			end,
+			desc = "Fuzzily search in current buffer",
+		},
+		-- quick save
+		-- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+	},
 }
--- plugins/init.lua
+++ plugins/init.lua
@@ -1,13 +1,20 @@
 return {
-  ["folke/tokyonight.nvim"] = {},
-  ["nvim-neorg/neorg"] = {
-    -- tag = "0.0.12",
-    config = function() require "user.plugins.neorg" end,
-    requires = "nvim-neorg/neorg-telescope",
-  },
-  ["kevinhwang91/rnvimr"] = { cmd = "RnvimrToggle", config = function() require "user.plugins.ranger" end },
-  ["dstein64/vim-startuptime"] = {},
+	["folke/tokyonight.nvim"] = {},
+	["nvim-neorg/neorg"] = {
+		tag = "0.0.12",
+		config = function()
+			require("user.plugins.neorg")
+		end,
+		requires = "nvim-neorg/neorg-telescope",
+	},
+	["kevinhwang91/rnvimr"] = {
+		cmd = "RnvimrToggle",
+		config = function()
+			require("user.plugins.ranger")
+		end,
+	},
+	["dstein64/vim-startuptime"] = {},
 
-  -- ["windwp/nvim-autopairs"] = { disable = true },
-  -- {},
+	-- ["windwp/nvim-autopairs"] = { disable = true },
+	-- {},
 }
